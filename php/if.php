1bf11f11af46f24c533318a8a72ad11a7af378fa
Starnes81 <starnes81@gmail.com>
2014-02-06 14:42:35 -0600

    Complete added else and else if
    added else and if else statements

--- php/if.php
+++ php/if.php
@@ -9,34 +9,58 @@ $c = '10';
 
 if ($a < $b) {
 	echo "$a is less than $b\n";
+} else {
+	echo "$a is greater than $b\n";
 }
 
 if ($b > $a) {
 	echo "$b is greater than $a\n";
+} else {
+	echo "$b is less than $a\n";
 }
 
 if ($b >= $c) {
 	echo "$b is greater than or equal to $c\n";
+} else  {
+	echo "$b is less than or equal to $c\n";
 }
 
 if ($b <= $c) {
 	echo "$b is less than or equal to $c\n";
+} else  {
+	echo "$b is greater than or equal to $c\m";
 }
 
 if ($b == $c) {
 	echo "$b is equal to $c\n";
+} else if ($b === $C) {
+	echo "$b is not identical to $c\n";
+} else { "$b is identical to $c\n";
+
 }
 
 if ($b === $c) {
 	echo "$b is not identical to $c\n";
-}
+} else  if ($b == $c) {
+	echo "$b is not equal to $c\n";
+} else{
+	echo "$b is equal to $c\n";
 
+}
 if ($b != $c) {
 	echo "$b is not equal to $c\n";
+} else if ($b !== $c) {
+	echo "$b is not identical to $c\n";
+} else {
+	echo "$b is identical to $c\n";
 }
 
 if ($b !== $c) {
 	echo "$b is not identical to $c\n";
+} else if ($b != $c) {
+	echo "$b is not equal to $c\n";
+} else {
+	echo "$b is equal to $c\n";
 }
 
 ?>
