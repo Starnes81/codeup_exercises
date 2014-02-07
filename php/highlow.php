84574ea059a36207acc35eecc99c10c229342a7a
Starnes81 <starnes81@gmail.com>
2014-02-07 11:11:16 -0600

    high low game
    added rand and player guess

--- php/highlow.php
+++ php/highlow.php
@@ -8,12 +8,20 @@ fwrite(STDOUT, 'What is your name?' );
 $first_name = fgets(STDIN);
 //say hello
 
-fwrite(STDOUT, "Hello $first_name\n")
+fwrite(STDOUT, "Hello $first_name\n");
 
 //rand number
 
+mt_rand(1,100);
+
+//guess
+
+fwrite(STDOUT, 'Guess a number between 1 and 100!');
+
+$guess_number = fgets(STDIN);
 
 //if
+
 
 
 //elseif
