bd9eaa78e0b2e7116a6be9978022790e506f75bb
Starnes81 <starnes81@gmail.com>
2014-02-07 11:05:58 -0600

    high low game
    added logic and get player info

---
+++ php/highlow.php
@@ -0,0 +1,25 @@
+
+<?php
+//high low game
+
+fwrite(STDOUT, 'What is your name?' );
+//input name
+
+$first_name = fgets(STDIN);
+//say hello
+
+fwrite(STDOUT, "Hello $first_name\n")
+
+//rand number
+
+
+//if
+
+
+//elseif
+
+
+else
+
+
+//exit
