d8b9221b921c4b66d81112a988ae31ee18538b14
Starnes81 <starnes81@gmail.com>
2014-02-10 08:36:20 -0600

    Added is_numeric
    No errors

--- php/for.php
+++ php/for.php
@@ -2,11 +2,24 @@
 // prompt user for start and end number.
 fwrite(STDOUT, 'Pick a starting number!');
 
-$starting_number = fgets(STDIN); 
+$starting_number = fgets(STDIN);
+
+if (!is_numeric($starting_number)) {
+	echo " '{$starting_number}' is numeric ", PHP_EOL;
+} else  {
+		echo " '{$starting_number}' is NOT numeric ", PHP_EOL;	
+}
+
 
 fwrite(STDOUT, 'Pick a endinging number!');
 
-$ending_number = fgets(STDIN); 
+$ending_number = fgets(STDIN);
+
+if (!is_numeric($ending_number)) {
+	echo " '{$ending_number}' is numeric ", PHP_EOL;
+} else  {
+	echo " '{$ending_number}' is NOT numeric ", PHP_EOL;
+}
 
 //add user increment
 
