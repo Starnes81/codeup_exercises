cb067a40396e3fd06b3930e9025c0a63e18b2cbc
Starnes81 <starnes81@gmail.com>
2014-02-10 15:43:33 -0600

    switch lesson
    completed using rand

--- php/switch.php
+++ php/switch.php
@@ -7,16 +7,31 @@ date_default_timezone_set('America/Chicago');
 
 // Get Day of Week as number
 // 1 (for Monday) through 7 (for Sunday)
-$day_of_week = date('N');
-
+//$day_of_week = date('N');
+$day_of_week = rand(1,7);
 switch($day_of_week) {
     case 1:
         // Output Monday
+    	echo "Monday\n";
+    	break;
     case 2: 
         // Output Tuesday
+    	echo "Tuesday\n";
+    	break;
+    case 3:
+    	echo "Wednesday\n";
+    	break;
+    case 4: 
+    	echo "Thursday\n";
+    	break;
+    case 5:
+    	echo "Friday\n";
+    	break;
+    default:
+    	echo "Weekend\n";
     //etc
 
-
+}
 
 
 
