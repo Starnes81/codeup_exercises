e27aa7f2d19deca509a907d6a5fbcdfd14bbc775
Starnes81 <starnes81@gmail.com>
2014-02-10 15:10:30 -0600

    Switch exercise
    created file, copied over code.

---
+++ php/switch.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+// Set the default timezone
+date_default_timezone_set('America/Chicago');
+
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$day_of_week = date('N');
+
+switch($day_of_week) {
+    case 1:
+        // Output Monday
+    case 2: 
+        // Output Tuesday
+    //etc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
