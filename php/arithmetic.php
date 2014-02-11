1f6f696d3a647f36b871e33fc0b4d5cbacfca1df
Starnes81 <starnes81@gmail.com>
2014-02-11 15:02:55 -0600

    arithmetic functions return complete
    completed returns, no echo statements

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -27,43 +27,40 @@ function add($a, $b = 0) {
 //subtract
 function subtract($a, $b = 0) {
 	if (paramAreValid($a, $b)) {
-	return $a - $b;
+		return $a - $b;
     } else {
-    return 0;
+    	return 0;
     }
 }
 
 
 //multiply
 function multiply($a, $b = 0) {
-	var_dump($a);
 	if (paramAreValid($a, $b)) {
-	return $a * $b;
+		return $a * $b;
     } else {
-    return 0;
+   	 	return 0;
     }
 }
 
 //divide
 function divide($a, $b = 0) {
-	 if (paramAreValid($a, $b)) {
-    	echo "You can not divide 0 by 0!\n";
-	} elseif (is_numeric($a) && is_numeric($b)) {
-		echo $a / $b . PHP_EOL;	
-	} else {
-		validate();
-	}
+	if ($b == 0) {
+		echo "ERROR can not divide 0!\n";
+	} elseif (paramAreValid($a, $b)) {
+		return $a / $b; {
+	} 
 }
 
 //modulus
 function modulus($a , $b = 0) {
 	if (paramAreValid($a, $b)) {
-	return $a % $b;
+		return $a % $b;
     } else {
-    return 0;
+    	return 0;
     } 
+	}
 }
-
 
 add('Steve', 10);
 subtract('dog', 0);
