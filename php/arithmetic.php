29721baa0b8182e6f4b92502bd90c6532b19b392
Starnes81 <starnes81@gmail.com>
2014-02-11 12:57:00 -0600

    arithmetic functions 2
    added error for attempts to divied by zero

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -6,52 +6,63 @@
 
 //add
 
-function add($a, $b) {
+function add($a, $b = 0) {
 	if (is_numeric($a) && is_numeric($b)){
 	echo $a + $b . PHP_EOL;	
 	} else {
 		echo "Input must be numeric!\n";
+    } if ($a || $b = 0) {
+    	echo "You can not divide 0 by 0!\n";
 	}
 }
 add('Steve', 10);
 
 //subtract
-function subtract($a, $b) {
+function subtract($a, $b = 0) {
 	if (is_numeric($a) && is_numeric($b)) {
 	echo $a - $b . PHP_EOL;
     } else {
     	echo "Input must be numeric!\n";
-    }
+    } if ($a || $b = 0) {
+    	echo "You can not divide 0 by 0!\n";
+	}
 }
-subtract('Dog', 15);
+subtract('dog', 0);
+
 
 //multiply
-function multiply($a, $b) {
+function multiply($a, $b = 0) {
 	if (is_numeric($a) && is_numeric($b)) {
 	echo $a * $b . PHP_EOL;
     } else {
     	echo "Input must be numeric!\n";
-    }
+    } if ($a || $b = 0) {
+    	echo "You can not divide 0 by 0!\n";
+	}
 }
 multiply('cat', 40.405);
 
 //divide
-function divide($a, $b) {
+function divide($a, $b = 0) {
 	if (is_numeric($a) && is_numeric($b)) {
 	echo $a / $b . PHP_EOL;	
 	} else {
 		echo "Input must be numeric!\n";
+	} if ($a || $b = 0) {
+    	echo "You can not divide 0 by 0!\n";
 	}
 }
 divide("HELLO", 30);
 
 //modulus
-function modulus($a , $b) {
+function modulus($a , $b = 0) {
 	if (is_numeric($a) && is_numeric($b)) {
 	echo ($a % $b) . PHP_EOL;
     } else {
     	echo "Input must be numeric!\n";
-    }
+    } if ($a || $b = 0) {
+    	echo "You can not divide 0 by 0!\n";
+	}
 }
 modulus(60, 'MATH');
 
