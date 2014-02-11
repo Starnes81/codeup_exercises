ff7f72b1e518cadc19d087e41dbf9952bc166317
Starnes81 <starnes81@gmail.com>
2014-02-11 11:01:27 -0600

    arithmetic function 2
    added function

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -4,18 +4,26 @@
 
 //creating function exercises
 
-
 //add
 
+function add($a, $b) {
+	echo ($a, $b) . PHP_EOL;
+}
 
 //subtract
-
+function subtract($a, $b) {
+	echo ($a, $b) . PHP_EOL;
+}
 
 //multiply
-
+function multiply($a, $b) {
+	echo ($a, $b) . PHP_EOL;
+}
 
 //divide
-
+function divide($a, $b) {
+	echo ($a, $b) . PHP_EOL;
+}
 
 
 //modulus
