fdd6bc8d2b99381929dbd095e9b591a8012f9ac2
Starnes81 <starnes81@gmail.com>
2014-02-11 11:06:04 -0600

    arithmetic lesson 2
    tested functions adding modules

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -7,24 +7,24 @@
 //add
 
 function add($a, $b) {
-	echo ($a, $b) . PHP_EOL;
+	echo ($a + $b) . PHP_EOL;
 }
-
+add(5, 10);
 //subtract
 function subtract($a, $b) {
-	echo ($a, $b) . PHP_EOL;
+	echo ($a - $b) . PHP_EOL;
 }
-
+subtract(30, 15);
 //multiply
 function multiply($a, $b) {
-	echo ($a, $b) . PHP_EOL;
+	echo ($a * $b) . PHP_EOL;
 }
-
+multiply(60, 15);
 //divide
 function divide($a, $b) {
-	echo ($a, $b) . PHP_EOL;
+	echo ($a / $b) . PHP_EOL;
 }
-
+divide(50, 20);
 
 //modulus
 
