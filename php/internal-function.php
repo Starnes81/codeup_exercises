6d5d9332f076f80f1bf65a6627974f3102ea9261
Starnes81 <starnes81@gmail.com>
2014-02-11 20:31:24 -0600

    internal-functions
    tested function is set/empty un set not empty

--- php/internal-function.php
+++ php/internal-function.php
@@ -8,23 +8,24 @@ $array = array(1,2,3);
 
 // Create a funciton that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
 function varCheck($var, $var_name) {
-	if (empty($variable)) {
+	if (empty($var_name)) {
 		echo "$var_name is EMPTY" . PHP_EOL;
 	} else {
-		echo "$var_name is Not EMPTY";
+		echo "$var_name is Not EMPTY" . PHP_EOL;
 	}
 	if (isset($var_name)) {
-		echo "$var_name is SET";
+		echo "$var_name is SET" . PHP_EOL;
 	} else {
-		echo "$var_name is Not SET"
+		echo "$var_name is Not SET" . PHP_EOL;
     }
 }
-// TEST: If var $nothing is set, display '$nothing is SET'
 
+// TEST: If var $nothing is set, display '$nothing is SET'
 // TEST: If var $nothing is empty, display '$nothing is EMPTY'
+echo varCheck($nothing, '$nothing');
 
 // TEST: If var $something is set, display '$something is SET'
-
+echo varCheck($something, '$something');
 // Serialize the array $array, and output the results
 
 // Unserialize the array $array, and output the results
